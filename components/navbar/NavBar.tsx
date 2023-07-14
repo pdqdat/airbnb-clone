ae4dbcfdf5f39c6a539cebe9d53075ed828f80b5
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-14 22:42:04 +0700

    styled navbar

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NavBar = () => {
-    return <div>NavBar</div>;
+    return <div className="fixed w-full bg-white z-10 shadow-sm">NavBar</div>;
 };
 
 export default NavBar;
