02498d68b995c7242a7927780525c902a1ae4604
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-14 22:50:05 +0700

    added container component

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
+import Container from "../Container";
+
 const NavBar = () => {
-    return <div className="fixed w-full bg-white z-10 shadow-sm">NavBar</div>;
+    return (
+        <div className="fixed w-full bg-white z-10 shadow-sm">
+            <div className="py-4 border-b-[1px]">
+                <Container>Dat Phan</Container>
+            </div>
+        </div>
+    );
 };
 
 export default NavBar;
