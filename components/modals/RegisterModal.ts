fa48ec2b66b25942a203f868394f57cc5f75d96c
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-15 13:10:57 +0700

    register modal

--- components/modals/RegisterModal.ts
+++ components/modals/RegisterModal.ts
@@ -2,7 +2,7 @@
 
 import { useCallback, useState } from "react";
 import axios from "axios";
-import { FieldValue, SubmitHandler, useForm } from "react-hook-form";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 // hooks
 import useRegisterModal from "@/hooks/useRegisterModal";
@@ -11,8 +11,52 @@ import useRegisterModal from "@/hooks/useRegisterModal";
 import { AiFillGithub } from "react-icons/ai";
 import { FcGoogle } from "react-icons/fc";
 
+// components
+import Modal from "./Modal";
+import { title } from "process";
+
 const RegisterModal = () => {
-    return <div>RegisterModal</div>;
+    const registerModal = useRegisterModal();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+        },
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        axios
+            .post("/api/register", data)
+            .then(() => {
+                registerModal.onClose();
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    };
+
+    return (
+        <Modal
+            title="Register"
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            actionLabel="Continue"
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+        />
+    );
 };
 
 export default RegisterModal;
