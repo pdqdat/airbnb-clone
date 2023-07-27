3bfe82505dc9fa11c8d0cd6e2cc042acb26d4196
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-07-27 16:41:18 +0700

    feat: airbnb my home - select category

--- components/modals/RentModal.tsx
+++ components/modals/RentModal.tsx
@@ -1,22 +1,123 @@
 "use client";
 
+import { useMemo, useState } from "react";
+import { useForm, FieldValues } from "react-hook-form";
+
 // components
 import Modal from "./Modal";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
 
 // hooks
 import useRentModal from "@/hooks/useRentModal";
+import Heading from "../Heading";
+
+enum STEPS {
+    CATEGORY = 0,
+    LOCATION = 1,
+    INFO = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4,
+    PRICE = 5,
+}
 
 const RentModal = () => {
     const rentModal = useRentModal();
 
+    const [step, setStep] = useState(STEPS.CATEGORY);
+
+    const {
+        register,
+        handleSubmit,
+        setValue,
+        watch,
+        formState: { errors },
+        reset,
+    } = useForm<FieldValues>({
+        defaultValues: {
+            category: "",
+            location: null,
+            guestCount: 1,
+            bathroomCount: 1,
+            imageSrc: "",
+            price: 1,
+            title: "",
+            description: "",
+        },
+    });
+
+    const category = watch("category");
+
+    const setCustomValue = (id: string, value: any) => {
+        setValue(id, value, {
+            shouldDirty: true,
+            shouldTouch: true,
+            shouldValidate: true,
+        });
+    };
+
+    const onBack = () => {
+        setStep((value) => value - 1);
+    };
+
+    const onNext = () => {
+        setStep((value) => value + 1);
+    };
+
+    const actionLabel = useMemo(() => {
+        // if user is on the last step, change the label to "Create"
+        if (step === STEPS.PRICE) {
+            return "Create";
+        }
+
+        return "Next";
+    }, [step]);
+
+    const secondaryActionLabel = useMemo(() => {
+        // if user is on the first step, hide the back button
+        if (step === STEPS.CATEGORY) {
+            return undefined;
+        }
+
+        return "Back";
+    }, [step]);
+
+    let bodyContent = (
+        <div className="flex flex-col gap-8">
+            <Heading
+                title="Which of these best describes your place?"
+                subtitle="Pick a category"
+            />
+
+            {/* categories */}
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+                {categories.map((item) => (
+                    <div key={item.label} className="col-span-1">
+                        {/* {item.label} */}
+                        <CategoryInput
+                            onClick={(category) =>
+                                setCustomValue("category", category)
+                            }
+                            selected={category === item.label}
+                            label={item.label}
+                            icon={item.icon}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+
     return (
         <Modal
             title="Airbnb your home"
-            // body={bodyContent}
+            body={bodyContent}
             // footer={footerContent}
             // disabled={isLoading}
             isOpen={rentModal.isOpen}
-            actionLabel="Submit"
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
             onClose={rentModal.onClose}
             onSubmit={rentModal.onClose}
         />
