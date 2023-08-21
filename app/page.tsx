c6ca94f1ad50b9393e5596af1e1932fc40d3e99f
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-08-21 10:59:23 +0700

    feat: working on listingCard

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@
 import ClientOnly from "@/components/ClientOnly";
 import Container from "@/components/Container";
 import EmptyState from "@/components/EmptyState";
+import ListingCard from "@/components/listings/ListingCard";
 
 // actions
 import getListing from "@/actions/getListing";
+import getCurrentUser from "@/actions/getCurrentUser";
 
 export default async function Home() {
     const listings = await getListing();
-    const isEmpty = true;
+    const currentUser = await getCurrentUser();
 
     if (listings.length === 0) {
         return (
@@ -22,10 +24,14 @@ export default async function Home() {
         <ClientOnly>
             <Container>
                 <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-                    {listings.map((listing:any)=>{
-                      return (
-                        <div>{listing.title}</div>
-                      )
+                    {listings.map((listing: any) => {
+                        return (
+                            <ListingCard
+                                currentUser={currentUser}
+                                key={listing.id}
+                                data={listing}
+                            />
+                        );
                     })}
                 </div>
             </Container>
---
+++ components/listings/ListingCard.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { Listing, Reservation } from "@prisma/client";
+
+import { SafeUser } from "@/types";
+import useCountries from "@/hooks/useCountries";
+import React, { useCallback, useMemo } from "react";
+import { format } from "date-fns";
+
+interface ListingCardProps {
+    data: Listing;
+    reservation?: Reservation;
+    onAction?: (id: string) => void;
+    disabled?: boolean;
+    actionLabel?: string;
+    actionId?: string;
+    currentUser?: SafeUser | null;
+}
+
+const ListingCard: React.FC<ListingCardProps> = ({
+    data,
+    reservation,
+    onAction,
+    disabled,
+    actionLabel,
+    actionId = "",
+    currentUser,
+}) => {
+    const router = useRouter();
+    const { getByValue } = useCountries();
+
+    const location = getByValue(data.lacationValue);
+
+    const handleCancel = useCallback(
+        (e: React.MouseEvent<HTMLButtonElement>) => {
+            e.stopPropagation();
+
+            if (disabled) {
+                return;
+            }
+
+            onAction?.(actionId);
+        },
+        [onAction, actionId, disabled]
+    );
+
+    const price = useMemo(() => {
+        if (reservation) {
+            return reservation.totalPrice;
+        }
+
+        return data.price;
+    }, [reservation, data.price]);
+
+    const reservationDate = useMemo(() => {
+        if (!reservation) {
+            return null;
+        }
+
+        const start = new Date(reservation.startDate);
+        const end = new Date(reservation.endDate);
+
+        return `${format(start, "PP")} - ${format(end, "PP")}`;
+    }, [reservation]);
+
+    return <div>ListingCard</div>;
+};
+
+export default ListingCard;
