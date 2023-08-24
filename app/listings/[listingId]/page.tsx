db492f3d3c5a6b34e62e7de361a2efdf6f254686
Dat Phan <80768235+QuocDat5@users.noreply.github.com>
2023-08-24 10:22:14 +0700

    feat: listing page prototype

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,8 +1,35 @@
-const ListingPage=()=>{
+import getListingById from "@/actions/getListingById";
+
+// components
+import ClientOnly from "@/components/ClientOnly";
+import EmptyState from "@/components/EmptyState";
+
+interface IParams{
+    listingId?:string;
+}
+
+const ListingPage=async ({params}:{params:IParams})=>{
+    const listing =await getListingById(params);
+
+    if (!listing){
+        return(
+            <ClientOnly>
+<EmptyState/>
+            </ClientOnly>
+        )
+    }
+
     return(
         <div>
-            Individual listing page.
+            {listing.title}
         </div>
+
+<ClientOnly>
+    <ListingClient
+listing={listing}
+currentUser={currentUser}
+    />
+            </ClientOnly>
     )
 }
 
